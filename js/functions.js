47f0c305fa47c8eef51ad243798180000cbe266c
Illy85 <soaccentru@gmail.com>
2023-06-12 23:17:16 +0200

    Code refactoring

--- js/functions.js
+++ js/functions.js
@@ -1,27 +1,24 @@
-function getStringLength(string, maxLength) {
-  return string.length <= maxLength;
-}
+const getStringLength = (string, maxLength) => string.length <= maxLength;
 getStringLength ('I am happy', 10);
 
-function checkPalindrome(string) {
-
-  const newString = string.toUpperCase().replaceAll(' ', '');
-
-  let emptyString = '';
-  for (let i = newString.length - 1; i >= 0; i--) {
-    emptyString += newString.at(i);
+const checkPalindrome = (string) => {
+  let check = true;
+  string = string.toUpperCase().replaceAll(' ', '');
+  for (let i = 0; i < string.length; i++) {
+    if (string.at(i) !== string.at(string.length - i - 1)) {
+      check = false;
+      break;
+    }
   }
-  return (emptyString === newString) ? 'It is a palindrome' : 'It is not a palindrome';
-}
-checkPalindrome('Murder FOR a jar OF red rum');
+  return check;
+};
+checkPalindrome('Murder fOR a jar oF red rum');
 
 function getNumber(string) {
-
   let number = string.replace(/[^0-9]/g, '');
   for (let i = 0; i < string.length; i++) {
     number += parseInt(string, 10);
   }
   return parseInt(number, 10);
 }
-
 getNumber('Raw meat should be stored between -0,5°C and +2.6°C');
