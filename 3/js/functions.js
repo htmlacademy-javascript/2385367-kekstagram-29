2848746978f959b9a62ab3e5f380e521dc1495c7
Keks <keks@htmlacademy.ru>
2023-06-17 14:56:32 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/functions.js
@@ -0,0 +1,24 @@
+const getStringLength = (string, maxLength) => string.length <= maxLength;
+getStringLength ('I am happy', 10);
+
+const checkPalindrome = (string) => {
+  let check = true;
+  string = string.toUpperCase().replaceAll(' ', '');
+  for (let i = 0; i < string.length; i++) {
+    if (string.at(i) !== string.at(string.length - i - 1)) {
+      check = false;
+      break;
+    }
+  }
+  return check;
+};
+checkPalindrome('Murder fOR a jar oF red rum');
+
+function getNumber(string) {
+  let number = string.replace(/[^0-9]/g, '');
+  for (let i = 0; i < string.length; i++) {
+    number += parseInt(string, 10);
+  }
+  return parseInt(number, 10);
+}
+getNumber('Raw meat should be stored between -0,5°C and +2.6°C');
