96077e422b41020b50e90d41f03a6f22fe447dfe
Illy85 <soaccentru@gmail.com>
2023-06-21 22:44:11 +0200

    Edited some codes

--- js/data.js
+++ js/data.js
@@ -1,31 +1,37 @@
-import {randomNames, commentMessages, imageDescriptions} from './mocks.js';
 import {getRandomInteger, createIdGenerator, getRandomArrayElement} from './util.js';
+import {getName, getComment, getDescription} from './mocks.js';
+
+const randomName = getName().randomNames;
+
+const commentMessage = getComment().commentMessages;
+
+const imageDescription = getDescription().imageDescriptions;
+
+const objectCount = 25;
 
 const generateCommentId = createIdGenerator ();
 
-const similarCommentCount = 25;
-
-const createComments = () => ({
+const createComment = () => ({
   id: generateCommentId (),
   avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
-  message: getRandomArrayElement(commentMessages),
-  name: getRandomArrayElement(randomNames),
+  message: getRandomArrayElement(commentMessage),
+  name: getRandomArrayElement(randomName),
 });
 
-const similarComments = Array.from({length: similarCommentCount}, createComments);
+const similarComments = Array.from({length: objectCount}, createComment);
 // eslint-disable-next-line no-console
 console.log(similarComments);
 
-const createCommentCards = () => {
-  const commentsArray = Array.from({length: getRandomInteger(0, 30)}, createComments);
+const createCommentCard = () => {
+  const commentsArray = Array.from({length: getRandomInteger(0, 30)}, createComment);
   return {
     id: getRandomInteger(1, 25),
     url: `photos/-${getRandomInteger(1, 25)}.jpg`,
-    description: getRandomArrayElement(imageDescriptions),
+    description: getRandomArrayElement(imageDescription),
     likes: getRandomInteger(15, 200),
     comments: commentsArray,
   };
 };
 
-const cardsArray = Array.from({length: similarCommentCount}, createCommentCards);
+const cardsArray = Array.from({length: objectCount}, createCommentCard);
 export {cardsArray};
--- js/util.js
+++ js/util.js
@@ -1,15 +1,14 @@
-const getRandomInteger = (a, b) => {
+export const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
 };
-const createIdGenerator = () => {
+export const createIdGenerator = () => {
   let lastGeneratedId = 0;
   return function () {
     lastGeneratedId += 1;
     return lastGeneratedId;
   };
 };
-const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
-export {getRandomInteger, createIdGenerator, getRandomArrayElement};
+export const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
