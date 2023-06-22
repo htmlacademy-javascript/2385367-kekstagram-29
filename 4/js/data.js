d1e7d72b0de29ad340d5c752b9283b078030ea80
Keks <keks@htmlacademy.ru>
2023-06-22 20:32:53 +0300

    :heavy_check_mark: Сборка #4

--- 4/js/data.js
+++ 4/js/data.js
@@ -1,31 +1,37 @@
-import {randomNames, commentMessages, imageDescriptions} from './mocks.js';
 import {getRandomInteger, createIdGenerator, getRandomArrayElement} from './util.js';
+import {getData} from './mocks.js';
+
+const randomName = getData().randomNames;
+
+const commentMessage = getData().commentMessages;
+
+const imageDescription = getData().imageDescriptions;
+
+const objectCount = 25;
 
 const generateCommentId = createIdGenerator ();
 
-const similarCommentCount = 25;
-
-const createComments = () => ({
+const createComment = () => ({
   id: generateCommentId (),
   avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
-  message: getRandomArrayElement(commentMessages),
-  name: getRandomArrayElement(randomNames),
+  message: getRandomArrayElement(commentMessage),
+  name: getRandomArrayElement(randomName),
 });
 
-const similarComments = Array.from({length: similarCommentCount}, createComments);
+const similarComments = Array.from({length: objectCount}, createComment);
 // eslint-disable-next-line no-console
 console.log(similarComments);
 
-const createCommentCards = () => {
-  const commentsArray = Array.from({length: getRandomInteger(0, 30)}, createComments);
+const createCommentCard = () => {
+  const commentsArray = Array.from({length: getRandomInteger(0, 30)}, createComment);
   return {
     id: getRandomInteger(1, 25),
     url: `photos/-${getRandomInteger(1, 25)}.jpg`,
-    description: getRandomArrayElement(imageDescriptions),
+    description: getRandomArrayElement(imageDescription),
     likes: getRandomInteger(15, 200),
     comments: commentsArray,
   };
 };
 
-const cardsArray = Array.from({length: similarCommentCount}, createCommentCards);
+const cardsArray = Array.from({length: objectCount}, createCommentCard);
 export {cardsArray};
