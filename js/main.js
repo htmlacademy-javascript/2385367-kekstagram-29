cfe613e0ee8522c74354efb0fab503838d9ba351
Illy85 <soaccentru@gmail.com>
2023-07-14 23:01:51 +0200

    Minor changes on validation form

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
-import { cardsArray } from './cards-data.js';
-
-import { renderGallery } from './gallery.js';
-renderGallery(cardsArray());
-
 import './loading-form.js';
 import './validation.js';
+import { cardsArray } from './cards-data.js';
+import { renderGallery } from './gallery.js';
+renderGallery(cardsArray());
