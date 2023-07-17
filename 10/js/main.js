39b463bc05aeadeea8061e2b3e013d01af71f877
Keks <keks@htmlacademy.ru>
2023-07-17 03:14:51 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/main.js
@@ -0,0 +1,5 @@
+import { cardsArray } from './cards-data.js';
+import { renderGallery } from './gallery.js';
+import './validation-loading-form.js';
+
+renderGallery(cardsArray());
---
+++ 10/js/validation-loading-form.js
@@ -0,0 +1,50 @@
+import { formValidator } from './input-validation.js';
+import { resetEffect } from './set-picture-effects.js';
+import { resetScale } from './picture-scale.js';
+
+const pictureLoadingForm = document.querySelector('.img-upload__form');
+const upLoadButton = pictureLoadingForm.querySelector('.img-upload__input');
+const popupLoading = pictureLoadingForm.querySelector('.img-upload__overlay');
+
+const open = (form) => {
+  form.classList.remove('hidden');
+  form.addEventListener('click', onFormCancelButtonClick);
+  document.body.classList.add('modal-open');
+  document.addEventListener('keydown', onFormKeydown);
+};
+
+upLoadButton.addEventListener('change', () => open(popupLoading));
+
+const close = (form) => {
+  form.classList.add('hidden');
+  form.removeEventListener('click', onFormCancelButtonClick);
+  document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onFormKeydown);
+};
+
+function onFormCancelButtonClick (evt) {
+  if (evt.target.closest('.cancel')) {
+    close(evt.currentTarget);
+  }
+}
+
+const isTextInFormFocused = (item) => (item.tagName === 'INPUT' && item.getAttribute('type') === 'text') || item.tagName === 'TEXTAREA';
+
+function onFormKeydown (evt) {
+  if (evt.key === 'Escape' && !isTextInFormFocused(document.activeElement)) {
+    document.querySelector('.img-upload__overlay:not(.hidden)').dispatchEvent(new Event('popup::hide'));
+    close(document.querySelector('.img-upload__overlay:not(.hidden)'));
+  }
+}
+
+pictureLoadingForm.addEventListener('reset', () => {
+
+  formValidator.reset();
+  resetScale();
+  resetEffect();
+
+});
+
+pictureLoadingForm.addEventListener('popup::hide', () => {
+  pictureLoadingForm.reset();
+}, true);
